0ecbfc6d87b2e6289e09dee8858035a73579b9da
Peter Dada <dapetoo@gmail.com>
2022-10-27 01:35:02 +0100

    Target group

--- alb.tf
+++ alb.tf
@@ -10,7 +10,7 @@ resource "aws_lb" "ext-alb" {
     aws_subnet.public[1].id
   ]
 
-   tags = merge(
+  tags = merge(
     var.tags,
     {
       Name = "ACS-ext-alb"
@@ -19,4 +19,22 @@ resource "aws_lb" "ext-alb" {
 
   ip_address_type    = "ipv4"
   load_balancer_type = "application"
+}
+
+
+
+resource "aws_lb_target_group" "nginx-tgt" {
+  health_check {
+    interval            = 10
+    path                = "/healthstatus"
+    protocol            = "HTTPS"
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+  name        = "nginx-tgt"
+  port        = 443
+  protocol    = "HTTPS"
+  target_type = "instance"
+  vpc_id      = aws_vpc.main.id
 }
