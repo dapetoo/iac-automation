ea571b516e93b7750d3c067e8ef3ef281c2fe077
dapetoo <dapetoo@gmail.com>
2022-11-21 23:13:37 +0100

    Variables refactoring

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -41,10 +41,26 @@ variable "environment" {
   description = "Enviroment"
 }
 
-variable "ami" {
+variable "ami-bastion" {
   type        = string
   description = "AMI ID for the launch template"
 }
+
+variable "ami-nginx" {
+  type        = string
+  description = "AMI ID for the launch template"
+}
+
+variable "ami-sonar" {
+  type        = string
+  description = "AMI ID for the launch template"
+}
+
+variable "ami-web" {
+  type        = string
+  description = "AMI ID for the launch template"
+}
+
 
 
 variable "keypair" {
